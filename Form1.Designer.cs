bcf7e9cde7743dfba84bd648f6a4e9116143c438
Sompong JongmEEsUK <j_sompong@live.com>
2025-01-17 10:30:45 +0700

    Hex 8 bits and 16 bits conversion, 17 January 2025
    Hex 8 bits and 16 bits conversion, 17 January 2025

---
+++ Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace WFAnet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HEXlistBox = new System.Windows.Forms.ListBox();
+            this.DEClistBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.S16blistBox = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.S16bDEClistBox = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // HEXlistBox
+            // 
+            this.HEXlistBox.FormattingEnabled = true;
+            this.HEXlistBox.Location = new System.Drawing.Point(88, 121);
+            this.HEXlistBox.Name = "HEXlistBox";
+            this.HEXlistBox.Size = new System.Drawing.Size(65, 212);
+            this.HEXlistBox.TabIndex = 0;
+            this.HEXlistBox.SelectedIndexChanged += new System.EventHandler(this.HEXlistBox_SelectedIndexChanged);
+            // 
+            // DEClistBox
+            // 
+            this.DEClistBox.FormattingEnabled = true;
+            this.DEClistBox.Location = new System.Drawing.Point(169, 121);
+            this.DEClistBox.Name = "DEClistBox";
+            this.DEClistBox.Size = new System.Drawing.Size(65, 212);
+            this.DEClistBox.TabIndex = 1;
+            this.DEClistBox.SelectedIndexChanged += new System.EventHandler(this.DEClistBox_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "8 Bits HEX";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(166, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "DEC";
+            // 
+            // S16blistBox
+            // 
+            this.S16blistBox.FormattingEnabled = true;
+            this.S16blistBox.Location = new System.Drawing.Point(368, 119);
+            this.S16blistBox.Name = "S16blistBox";
+            this.S16blistBox.Size = new System.Drawing.Size(65, 212);
+            this.S16blistBox.TabIndex = 4;
+            this.S16blistBox.SelectedIndexChanged += new System.EventHandler(this.S16blistBox_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(365, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "16 Bits HEX";
+            // 
+            // S16bDEClistBox
+            // 
+            this.S16bDEClistBox.FormattingEnabled = true;
+            this.S16bDEClistBox.Location = new System.Drawing.Point(454, 119);
+            this.S16bDEClistBox.Name = "S16bDEClistBox";
+            this.S16bDEClistBox.Size = new System.Drawing.Size(65, 212);
+            this.S16bDEClistBox.TabIndex = 6;
+            this.S16bDEClistBox.SelectedIndexChanged += new System.EventHandler(this.S16bDEClistBox_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(451, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "DEC";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.S16bDEClistBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.S16blistBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DEClistBox);
+            this.Controls.Add(this.HEXlistBox);
+            this.Name = "Form1";
+            this.Text = "Sine Wave HEX to DEC (VS Community 2022)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox HEXlistBox;
+        private System.Windows.Forms.ListBox DEClistBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox S16blistBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox S16bDEClistBox;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using CsvHelper;
+using System.Globalization;
+using static System.Console;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Collections;
+
+namespace WFAnet
+   
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+         //    public static string ReadAllText(string path);
+        InitializeComponent();
+            
+       
+        }
+        static byte[] HexStringToByteArray(string hex)
+        {
+            int length = hex.Length;
+            byte[] bytes = new byte[length / 2];
+            for (int i = 0; i < length; i += 2)
+            {
+                bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+            }
+            return bytes;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Define a 16-bit hexadecimal array                //  waveformsTable   // Sin wave
+            ushort[] hexArray = new ushort[]
+            {
+            0x7ff, 0x86a, 0x8d5, 0x93f, 0x9a9, 0xa11, 0xa78, 0xadd, 0xb40, 0xba1,
+            0xbff, 0xc5a, 0xcb2, 0xd08, 0xd59, 0xda7, 0xdf1, 0xe36, 0xe77, 0xeb4,
+        0xeec, 0xf1f, 0xf4d, 0xf77, 0xf9a, 0xfb9, 0xfd2, 0xfe5, 0xff3, 0xffc,
+        0xfff, 0xffc, 0xff3, 0xfe5, 0xfd2, 0xfb9, 0xf9a, 0xf77, 0xf4d, 0xf1f,
+        0xeec, 0xeb4, 0xe77, 0xe36, 0xdf1, 0xda7, 0xd59, 0xd08, 0xcb2, 0xc5a,
+        0xbff, 0xba1, 0xb40, 0xadd, 0xa78, 0xa11, 0x9a9, 0x93f, 0x8d5, 0x86a,
+        0x7ff, 0x794, 0x729, 0x6bf, 0x655, 0x5ed, 0x586, 0x521, 0x4be, 0x45d,
+        0x3ff, 0x3a4, 0x34c, 0x2f6, 0x2a5, 0x257, 0x20d, 0x1c8, 0x187, 0x14a,
+        0x112, 0xdf, 0xb1, 0x87, 0x64, 0x45, 0x2c, 0x19, 0xb, 0x2,
+        0x0, 0x2, 0xb, 0x19, 0x2c, 0x45, 0x64, 0x87, 0xb1, 0xdf,
+        0x112, 0x14a, 0x187, 0x1c8, 0x20d, 0x257, 0x2a5, 0x2f6, 0x34c, 0x3a4,
+        0x3ff, 0x45d, 0x4be, 0x521, 0x586, 0x5ed, 0x655, 0x6bf, 0x729, 0x794
+            };
+
+            // Print the array elements
+            foreach (ushort hexValue in hexArray)
+            {
+              //  MessageBox.Show($"0x{hexValue:X4}");
+                S16blistBox.Items.Add($"0x{hexValue:X4}");
+            }
+          
+            int itemCount = S16blistBox.Items.Count;                  // Count the items
+          //  Console.WriteLine($"Total items: {itemCount}");
+            for (int i = 0; i < itemCount; i++)
+            { 
+                string HjexValue = S16blistBox.Items[i].ToString();
+                int decimaValue = Convert.ToInt32(HjexValue, 16);
+                S16bDEClistBox.Items.Add(decimaValue);
+            }
+          
+            /* string hexString = "4A6F686E"; // Example hex string
+            byte[] byteArray = HexStringToByteArray(hexString);
+            MessageBox.Show("Byte Array:");
+            foreach (byte b in byteArray)
+            {
+                MessageBox.Show(b + " ");
+            }               */
+
+                string HexValue = S16blistBox.Items[0].ToString();
+                int decimalValue = Convert.ToInt32(HexValue, 16);
+            // Console.WriteLine($"The decimal value of 0x{hexValue} is {decimalValue}");
+            MessageBox.Show($"The decimal value of  {HexValue} is {decimalValue}");
+
+            // var fs = new FileStream(@"D:\WFAnet\bin\Debug\sinewaveHEX.csv", FileMode.Open);
+            var fs = new FileStream(@"D:\WFAnet\bin\Debug\sinewaveHEX1.csv", FileMode.Open);
+            var xreader = new StreamReader(fs);
+            while (!xreader.EndOfStream)
+            {
+                var line = xreader.ReadLine();
+              //  WriteLine(line?.ToUpperInvariant());
+                string hexdecim = line?.ToUpperInvariant();
+             //   MessageBox.Show(hexdecim);                  // แสดง เป็น กลุ่ม
+            }
+            xreader.Close();
+
+            //  จาก data :  อ่าน ฐาน 16 เป็น ฐาน 10            // Install-Package CsvHelper
+           //  var reader = new StreamReader("sinewaveHEX.csv");
+            var reader = new StreamReader("sinewaveHEX1.csv");
+
+            var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+            var records = csv.GetRecords<dynamic>();                         // read CSV file
+                                                                             // output
+            foreach (var r in records)
+            {
+                //WriteLine($"{r.FirstName,-15}{r.LastName,-10}{r.JoinedDate,15}{r.Salary,15}{r.Active,5}");
+                //WriteLine($"{r.HexValue,-15}");
+                //string hexdecim = $"{r.HexValue,-15}";
+                //   string hexdecim = $"{r.HexCol1}{r.HexCol2}{r.HexCol3}{r.HexCol4}";
+                string hexdecim = $"{r.HexColumn}";
+                //MessageBox.Show(hexdecim);
+                HEXlistBox.Items.Add(hexdecim);
+                int decValue = int.Parse(hexdecim, System.Globalization.NumberStyles.HexNumber);
+                string decim = Convert.ToString(decValue);
+                //  MessageBox.Show(decim);
+                DEClistBox.Items.Add(decim);
+            }
+            reader.Close();
+            //try
+            //{
+            //  Block of code to try
+            //}
+            //catch (Exception e)
+            //{
+            //  Block of code to handle errors
+            //}
+
+
+            try
+            {
+                //string contents = File.ReadAllText(@"C:\temp\readme1.txt");
+                string contents = File.ReadAllText(@"D:\WFAnet\bin\Debug\sinewaveHEX.csv");
+               // Console.WriteLine(contents);
+               //  MessageBox.Show(contents);              // แสดง เป็นกลุ่ม
+            }
+         //   catch (FileNotFoundException e)
+            catch (Exception)
+            {
+                //Console.WriteLine(e.Message);
+            }
+
+        }
+
+        private void HEXlistBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DEClistBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void S16blistBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void S16bDEClistBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
